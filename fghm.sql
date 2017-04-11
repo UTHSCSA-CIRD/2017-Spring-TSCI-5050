bd2bcb4b8272b47483b7e35a54372fcf0ddd2161
restrepojf <restreposuar@uthscsa.edu>
2017-04-11 10:23:56 -0500

    Saving local changes

--- fghm.sql
+++ fghm.sql
@@ -10,6 +10,7 @@ select sex, count(*) from fghm where sex is null;
 select hypertension, count(distinct id) from fghm group by hypertension;
 select hypertension, count(*) from fghm group by hypertension;
 select sex||";"|| obese from fghm;
+
 select sex, obese from fghm;
 select sex, obese from fghm where obese !="Normal";
 select sex, obese from fghm where obese !="Normal" and sex!="Female";
@@ -21,5 +22,34 @@ select obese, hypertension, dbp from fghm where obese="Normal" and hypertension=
 select obese, hypertension, sbp from fghm where obese="Normal" and hypertension!= "Normal" and sbp > 160;--bad result--
 select obese, hypertension, sbp from fghm where obese="Normal" and hypertension!= "Normal" and sbp > 160;--bad result--
 select obese, hypertension, sbp from fghm where obese="Normal" and hypertension!= "Normal" and sbp > "160";--bad result--
+
+---HWO2--How many diferent groups--
+select sex, count(*) from fghm group by sex;
+select obese, count(*) from fghm group by obese;
+select hypertension, count(*) from fghm group by hypertension;
+select chd, count(*) from fghm group by chd;
+--average, min and max of some variables-
 select min(sbp) from fghm;
 select max(sbp) from fghm;--bad result--
+update fghm set bmi = null where bmi = 'NA';
+select max(bmi) from fghm;
+select min(bmi) from fghm;
+select max (age) from fghm;
+select min(age) from fghm;
+select avg(age) from fghm;
+select avg (bmi) from fghm;
+select avg(sbp) from fghm;
+select avg(sbp) from fghm where sex="Male";
+select avg(sbp) from fghm where sex="Female";
+select avg(dbp) from fghm;
+select avg(scl) from fghm;--scl= serum cholesterol--
+select avg(scl) from fghm where sex="Male";
+select avg(scl) from fghm where sex="Female";
+--Is each subject measured one?--- yes
+select sex, count (distinct id) from fghm group by sex;
+select sex, count (*) from fghm group by sex;
+--Are some variables only allowed to have a few discrete , predefined value--
+select * from fghm;
+select age, count(*) from fghm;
+select age, count(*) from fghm where age is NULL;
+select age,count(*) from fghm where age is not NULL;
