e60ae9d9a37b54e5d06867240fd1487868809a2e
bokov@uthscsa.edu <bokov@uthscsa.edu>
2017-05-09 10:00:48 -0500

    Live edit

--- Session_08_Wrapup/Session_08.R
+++ Session_08_Wrapup/Session_08.R
@@ -7,5 +7,6 @@
 #+ echo=FALSE
 options(width=300);
 repo='https://cran.rstudio.com';
-if(!require(mgcv))) install.packages('mgcv',repos = repo);
-if(!require(faraway))) install.packages('faraway',repos = repo);
+if(!require(mgcv)) install.packages('mgcv',repos = repo);
+if(!require(faraway)) install.packages('faraway',repos = repo);
+if(!require(visreg)) install.packages('visreg',repos=repo);
