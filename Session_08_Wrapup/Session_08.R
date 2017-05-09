6ff97efea8b0af49469d2c8efd3187895041dc88
bokov@uthscsa.edu <bokov@uthscsa.edu>
2017-05-09 09:59:19 -0500

    Added session 8 file

---
+++ Session_08_Wrapup/Session_08.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Parting Thoughts"
+#' author: "Alex F. Bokov"
+#' date: "05/09/2017"
+#' output: html_document
+#' ---
+#+ echo=FALSE
+options(width=300);
+repo='https://cran.rstudio.com';
+if(!require(mgcv))) install.packages('mgcv',repos = repo);
+if(!require(faraway))) install.packages('faraway',repos = repo);
