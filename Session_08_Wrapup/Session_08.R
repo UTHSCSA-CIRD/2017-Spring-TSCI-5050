d7119d23bd7b46870de5b59a72a947a2e9053b23
bokov@uthscsa.edu <bokov@uthscsa.edu>
2017-05-09 10:05:53 -0500

    Live edit

--- Session_08_Wrapup/Session_08.R
+++ Session_08_Wrapup/Session_08.R
@@ -10,3 +10,13 @@ repo='https://cran.rstudio.com';
 if(!require(mgcv)) install.packages('mgcv',repos = repo);
 if(!require(faraway)) install.packages('faraway',repos = repo);
 if(!require(visreg)) install.packages('visreg',repos=repo);
+if(!require(ggplot2)) install.packages('ggplot2',repos=repo);
+if(!require(GGally)) install.package('GGally',repos=repo);
+#' Let's load the `ozone` dataset from the `faraway` package
+data(ozone);
+head(ozone);
+#' Visually explore
+ggpairs(transform(ozone,O3=log(O3),dO3=cut(O3,4)),
+        aes(color=dO3),upper=list(continuous='density'));
+#' Remember the linear model...
+o_lm <- lm(log(O3))
